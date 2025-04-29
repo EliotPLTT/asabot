1242838e08efee0e35b12ebbb953325ee07fdd87
Eliot Poulette <eliot.poulette@gmail.com>
2025-04-29 17:39:38 +0200

    Error Handling for solde

--- commands/utility/solde.js
+++ commands/utility/solde.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder, MessageFlags, EmbedBuilder } = require('discord.js');
 const { apiCall, formatDate } = require('../../utils.js');
 const { apiEndPoint } = require('../../config.json');
+//const { json } = require('express');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -20,9 +21,21 @@ module.exports = {
         const urlReq = apiEndPoint+"/db/STAT/solde/"+SID1+"/"+SID2;
         const jsonResponse = await apiCall(urlReq);
 
-        const embedToSend = embedFromJson(jsonResponse)
-        
-		await interaction.reply({ embeds: [embedToSend], flags: MessageFlags.Ephemeral });
+        console.log(jsonResponse);
+        console.log(jsonResponse.rows);
+        if (jsonResponse.rows.length  == 0){
+            const errorEmbed = new EmbedBuilder()
+            .setColor(0xFF0000) // Rouge pour une erreur
+            .setTitle('❌ Erreur')
+            .setDescription('Aucun transfert enregistré entre les '+SID1+' et les '+SID2)
+            .setFooter({ text: 'Veuillez réessayer plus tard.' })
+            .setTimestamp();
+            await interaction.reply({ embeds: [errorEmbed], flags: MessageFlags.Ephemeral })
+        }
+        else{
+            const embedToSend = embedFromJson(jsonResponse)
+		    await interaction.reply({ embeds: [embedToSend], flags: MessageFlags.Ephemeral })
+        }
 	},
 };
 
